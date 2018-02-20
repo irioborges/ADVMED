57176195bbb47b1e55c0a689a4f8d3ff346072e2
irioborges <iriormb@gmail.com>
2018-02-20 19:42:03 -0300

    Tirei a senha :)

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=bage3921_advmed',
-    'username' => 'bage3921_root',
-    'password' => 'q8s46ZdgT4',
+    'username' => 'username oculto',
+    'password' => 'senha oculta',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
